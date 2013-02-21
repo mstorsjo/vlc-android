4c074a6d02e465dae3a9dcfeebb4ed05c8c86629
Martin Storsjö <martin@martin.st>
2013-02-21 12:30:30 +0200

    Dump more symbols, for iomx/gralloc

--- dump-libs.sh
+++ dump-libs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in stagefright media utils binder; do
-	nm -D --defined-only lib$i.so | awk '{print $3}' | grep -v ^__aeabi | grep '\(IInterface\|OMXObserver\|MemoryDealer\|Binder\|RefBase\|String\|OMXClient\|IMemory\|^str\)' > lib$i.symbols
+for i in stagefright media utils binder cutils ui; do
+	nm -D --defined-only lib$i.so | awk '{print $3}' | grep -v ^__aeabi | grep '\(IInterface\|OMXObserver\|MemoryDealer\|Binder\|RefBase\|String\|OMXClient\|IMemory\|^str\|android_atomic\|GraphicBuffer\)' > lib$i.symbols
 done
 
