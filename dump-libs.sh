0f71b12197519ccf28d8dc15ca3fd0a18aedd79a
Martin Storsjö <martin@martin.st>
2013-02-25 00:34:55 +0200

    Include more symbols

--- dump-libs.sh
+++ dump-libs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 for i in stagefright media utils binder cutils ui; do
-	nm -D --defined-only lib$i.so | awk '{print $3}' | grep -v ^__aeabi | grep '\(IInterface\|OMXObserver\|MemoryDealer\|Binder\|RefBase\|String\|OMXClient\|IMemory\|^str\|android_atomic\|GraphicBuffer\)' > lib$i.symbols
+	nm -D --defined-only lib$i.so | awk '{print $3}' | grep -v ^__aeabi | grep '\(IInterface\|OMXObserver\|MemoryDealer\|Binder\|RefBase\|String\|OMXClient\|IMemory\|^str\|android_atomic\|GraphicBuffer\|SurfaceTexture\)' > lib$i.symbols
 done
 
